a11f364c50f66486dac74765c673be2bf422a433
Eugene Rodionov <git@theaqua.im>
2016-10-06 02:04:10 +0300

    fix: clean up navbar from react-router

--- src/components/navbar/brand.jsx
+++ src/components/navbar/brand.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router';
 import cn from 'classnames';
 
-export default (styles = {}) => ({ className, children, to, ...rest }) => {
-  if (to) {
-    return (
-      <Link {...rest} className={cn(styles.brand, className)} to={to}>
-        {children}
-      </Link>
-    );
-  } else {
-    return (
-      <a {...rest} className={cn(styles.brand, className)}>
-        {children}
-      </a>
-    );
-  }
+export default (styles = {}) => ({ className, children, ...rest }) => {
+  return (
+    <a {...rest} className={cn(styles.brand, className)}>
+      {children}
+    </a>
+  );
 };
--- src/components/navbar/link.jsx
+++ src/components/navbar/link.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router';
 import cn from 'classnames';
 
-const NavLink = (styles = {}) => ({ className, children, to, ...rest }) => {
-  if (to) {
-    return (
-      <Link {...rest} className={cn(styles.link, className)} to={to}>
-        {children}
-      </Link>
-    );
-  } else {
-    return (
-      <a {...rest} className={cn(styles.link, className)}>
-        {children}
-      </a>
-    );
-  }
+const NavLink = (styles = {}) => ({ className, children, ...rest }) => {
+  return (
+    <a {...rest} className={cn(styles.link, className)}>
+      {children}
+    </a>
+  );
 };
 
 export default NavLink;
